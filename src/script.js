d747562e13a80aef89270bd418c49de032f5cf23
puran dubey <dubeypuran2002@gmail.com>
2022-07-19 15:17:48 +0530

    This is an amazing web design.

---
+++ src/script.js
@@ -0,0 +1,7 @@
+gsap.from('.navbar',{duration:1.5,y:'-100%'})
+
+gsap.from('.leftTextHeader',{duration:1,x:'-120%',delay:1.3})
+gsap.from('.rightImgHeader',{duration:1,x:'100%',delay:1.8})
+
+gsap.from('.cardEkKeBadEk',{duration:1,opacity:0,delay:2.3,stagger:.3,ease:'power2.inOut'})
+
